5d97b9f2d6f2af30819c621617f400269372bb78
Parker EECS <parkeroth@gmail.com>
2012-09-04 13:30:12 -0500

    GPS Changes

--- src/com/example/maptest/Maps.java
+++ src/com/example/maptest/Maps.java
@@ -1,45 +1,57 @@
 package com.example.maptest;
 
-import java.util.List;
-
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
- 
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
-import com.google.android.maps.OverlayItem;
 
 public class Maps extends MapActivity {
 
-	private static final int latitudeE6 = 37985339;
-	private static final int longitudeE6 = 23716735;
-	
+	private MapController mapController;
+	private MapView mapView;
+	private LocationManager locationManager;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
 
-		MapView mapView = (MapView) findViewById(R.id.mapview);
-		mapView.setBuiltInZoomControls(true);
+		// get a hangle on the location manager
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, new LocationUpdateHandler());
 
-		List<Overlay> mapOverlays = mapView.getOverlays();
-		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher);
-		CustomItemizedOverlay itemizedOverlay = new CustomItemizedOverlay(
-				drawable, this);
+		// create a map view
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapController = mapView.getController();
+		mapController.setZoom(22);
+		setContentView(mapView);
+	}
 
-		GeoPoint point = new GeoPoint(latitudeE6, longitudeE6);
-		OverlayItem overlayitem = new OverlayItem(point, "Hello",
-				"I'm in Athens, Greece!");
+	public class LocationUpdateHandler implements LocationListener {
 
-		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
+		public void onLocationChanged(Location loc) {
+			int lat = (int) (loc.getLatitude() * 1E6);
+			int lng = (int) (loc.getLongitude() * 1E6);
+			GeoPoint point = new GeoPoint(lat, lng);
+			mapController.setCenter(point);
+			setContentView(mapView);
+		}
 
-		MapController mapController = mapView.getController();
-		mapController.animateTo(point);
-		mapController.setZoom(6);
+		public void onProviderDisabled(String provider) {
+		}
+
+		public void onProviderEnabled(String provider) {
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
 	}
 
 	@Override
